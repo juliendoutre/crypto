16ac0fd07d66de097358941bbc4166d5092d285d
Julien Doutre <jul.doutre@gmail.com>
2020-03-17 18:26:52 +0100

    Add AES CBC cipher

--- cryptolib/src/cipher.rs
+++ cryptolib/src/cipher.rs
@@ -1,5 +1,17 @@
 use super::xor;
-use openssl::{error::ErrorStack, symm};
+use openssl::symm;
+use std::{error::Error, fmt};
+
+#[derive(Debug)]
+pub struct EncryptionError;
+
+impl Error for EncryptionError {}
+
+impl fmt::Display for EncryptionError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Encryption error")
+    }
+}
 
 pub trait Cipher {
     fn encrypt(
@@ -7,13 +19,13 @@ pub trait Cipher {
         plaintext: &[u8],
         key: &[u8],
         iv: Option<&[u8]>,
-    ) -> Result<Vec<u8>, ErrorStack>;
+    ) -> Result<Vec<u8>, EncryptionError>;
     fn decrypt(
         &self,
         ciphertext: &[u8],
         key: &[u8],
         iv: Option<&[u8]>,
-    ) -> Result<Vec<u8>, ErrorStack>;
+    ) -> Result<Vec<u8>, EncryptionError>;
 }
 
 pub struct Xor;
@@ -36,7 +48,7 @@ impl Cipher for Xor {
         plaintext: &[u8],
         key: &[u8],
         _: Option<&[u8]>,
-    ) -> Result<Vec<u8>, ErrorStack> {
+    ) -> Result<Vec<u8>, EncryptionError> {
         Ok(self.repeating_xor(plaintext, key))
     }
 
@@ -45,7 +57,7 @@ impl Cipher for Xor {
         ciphertext: &[u8],
         key: &[u8],
         _: Option<&[u8]>,
-    ) -> Result<Vec<u8>, ErrorStack> {
+    ) -> Result<Vec<u8>, EncryptionError> {
         Ok(self.repeating_xor(ciphertext, key))
     }
 }
@@ -68,8 +80,11 @@ impl Cipher for AesEcb {
         plaintext: &[u8],
         key: &[u8],
         _: Option<&[u8]>,
-    ) -> Result<Vec<u8>, ErrorStack> {
-        symm::encrypt(self.cipher, key, None, plaintext)
+    ) -> Result<Vec<u8>, EncryptionError> {
+        match symm::encrypt(self.cipher, key, None, plaintext) {
+            Ok(ciphertext) => Ok(ciphertext),
+            Err(_) => Err(EncryptionError),
+        }
     }
 
     fn decrypt(
@@ -77,8 +92,11 @@ impl Cipher for AesEcb {
         ciphertext: &[u8],
         key: &[u8],
         _: Option<&[u8]>,
-    ) -> Result<Vec<u8>, ErrorStack> {
-        symm::decrypt(self.cipher, key, None, ciphertext)
+    ) -> Result<Vec<u8>, EncryptionError> {
+        match symm::decrypt(self.cipher, key, None, ciphertext) {
+            Ok(plaintext) => Ok(plaintext),
+            Err(_) => Err(EncryptionError),
+        }
     }
 }
 
@@ -99,18 +117,58 @@ impl Cipher for AesCbc {
         &self,
         plaintext: &[u8],
         key: &[u8],
-        _: Option<&[u8]>,
-    ) -> Result<Vec<u8>, ErrorStack> {
-        symm::encrypt(self.cipher, key, None, plaintext)
+        iv: Option<&[u8]>,
+    ) -> Result<Vec<u8>, EncryptionError> {
+        if key.len() != 16 || iv.is_none() || (iv.is_some() && iv.unwrap().len() != 16) {
+            return Err(EncryptionError);
+        }
+
+        if let Some(iv) = iv {
+            let mut ciphertext = Vec::<u8>::with_capacity(plaintext.len());
+
+            let mut c = iv.to_vec();
+            for p in plaintext.chunks(16) {
+                c = match symm::encrypt(self.cipher, key, None, &xor(p, &c)) {
+                    Ok(b) => b,
+                    Err(_) => return Err(EncryptionError),
+                };
+
+                ciphertext.extend(&c);
+            }
+
+            return Ok(ciphertext);
+        }
+
+        Err(EncryptionError)
     }
 
     fn decrypt(
         &self,
         ciphertext: &[u8],
         key: &[u8],
-        _: Option<&[u8]>,
-    ) -> Result<Vec<u8>, ErrorStack> {
-        symm::decrypt(self.cipher, key, None, ciphertext)
+        iv: Option<&[u8]>,
+    ) -> Result<Vec<u8>, EncryptionError> {
+        if key.len() != 16 || iv.is_none() || (iv.is_some() && iv.unwrap().len() != 16) {
+            return Err(EncryptionError);
+        }
+
+        if let Some(iv) = iv {
+            let mut plaintext = Vec::<u8>::with_capacity(ciphertext.len());
+
+            let previous_c = iv.to_vec();
+            for c in ciphertext.chunks(16) {
+                let decrypted_current_c = match symm::decrypt(self.cipher, key, None, c) {
+                    Ok(b) => b,
+                    Err(_) => return Err(EncryptionError),
+                };
+
+                plaintext.extend(&xor(&previous_c, &decrypted_current_c));
+            }
+
+            return Ok(plaintext);
+        }
+
+        Err(EncryptionError)
     }
 }
 
